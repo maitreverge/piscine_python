2a4a4e254ecdf14b0bc1c010523dfbfbdc136bda
maitreverge_fedora <verge.f@gmail.com>
2026-03-29 11:31:56 +0200

    add copy script

---
+++ module_00/copy_main.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def os_walk():
+    for root, dirs, files in os.walk('.'):
+        print("Current directory:", root)
+        print("Subdirectories:", dirs)
+        print("Files:", files)
+
+MAIN_FILE = "./main.py"
+def main():
+    # os_walk()
+
+    for _, dirs, _ in os.walk('.'):
+        for dir_name in dirs:
+            shutil.copy2(MAIN_FILE, dir_name)
+
+main()
