f1d1a37e2f65b954f9167716a00dd0692e1a6327
maitreverge_fedora <verge.f@gmail.com>
2026-03-29 11:35:45 +0200

    Fix script

--- module_00/copy_main.py
+++ module_00/copy_main.py
@@ -13,6 +13,6 @@ def main():
 
     for _, dirs, _ in os.walk('.'):
         for dir_name in dirs:
-            shutil.copy2(MAIN_FILE, dir_name)
+            shutil.copy2(MAIN_FILE, os.path.join(dir_name, "main.py"))
 
 main()
