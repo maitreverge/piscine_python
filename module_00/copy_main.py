894ce83385397de747307237e17f5be7fe95a0a7
maitreverge_fedora <verge.f@gmail.com>
2026-03-29 11:42:30 +0200

    oui oui baguette

--- module_00/copy_main.py
+++ module_00/copy_main.py
@@ -5,6 +5,9 @@ MAIN_FILE = "./main.py"
 EXCLUDED_DIRS = {"__pycache__", ".mypy_cache", "mypy_cache"}
 
 def main():
+    """
+    Copy the `main.py` in all subfolders.
+    """
     for root, dirs, _ in os.walk("."):
         dirs[:] = [d for d in dirs if d not in EXCLUDED_DIRS]
 
