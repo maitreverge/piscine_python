1d3e4a6159db84e34fde7219a40cf0deb97ec477
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-04 00:40:22 +0200

    ya des fonctions non authorisees mais tu vas faire quoi ?

---
+++ module_03/ex0/ft_command_quest.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+import sys
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    print("=== Command Quest ===")
+    print(
+        f"Program name: {sys.argv[0][2:]}"
+    )  # Strip the `./` at the beggining
+
+    # ! Alternate solution could be
+    # print(f"Program name: {__file__.split('/')[-1]}")
+
+    lenght_args: int = len(sys.argv)
+
+    if lenght_args == 2:
+        print("No argument provided !")
+    else:
+        print(f"Arguments recieved : {lenght_args - 1}")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"Argument {i}: {arg}")
+
+    print(f"Total arguments : {lenght_args}")
+
+
+if __name__ == "__main__":
+    main()
