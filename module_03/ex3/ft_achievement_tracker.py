66adce60e2979142e853f53e5ff6058050080ff6
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-04 12:59:01 +0200

    Ex 03 done

--- module_03/ex3/ft_achievement_tracker.py
+++ module_03/ex3/ft_achievement_tracker.py
@@ -11,15 +11,15 @@ Syntax : {}
 
 
 def gen_player_achievements() -> set[str]:
-    all_achivements: list[str] = [
+    all_achievements: list[str] = [
         "Crafting Genius",
         "Strategist",
         "World Savior",
         "Speed Runner",
-        # "Survivor",
-        # "Master Explorer",
-        # "Treasure Hunter",
-        # "Unstoppable",
+        "Survivor",
+        "Master Explorer",
+        "Treasure Hunter",
+        "Unstoppable",
         # "First Steps",
         # "Collector Supreme",
         # "Untouchable",
@@ -27,49 +27,70 @@ def gen_player_achievements() -> set[str]:
         # "Boss Slayer",
     ]
 
-    rdm_number = random.randint(1, len(all_achivements))
+    rdm_number = random.randint(1, len(all_achievements))
 
     # * Note : random.sample only accept iterables objects such a lists.
     # * k == number of randomly selected items.
     # * set() constructor has been used because `random.sample` returns a list
-    return set(random.sample(all_achivements, k=rdm_number))
+    return set(random.sample(all_achievements, k=rdm_number))
+
 
 def display_stats(persons: dict[str, set[str]]) -> None:
-    # for person, achivement in persons.items():
-    #     print(f"Player {person.capitalize()}: {achivement}")
+    # for person, achievement in persons.items():
+    #     print(f"Player {person.capitalize()}: {achievement}")
 
     # * Note : `*` operator stands for unpacking
     # `set.union` implies at least one arguments, crashes if dict empty
     # `set().union` creates a emptyset first, so does not crash if dict is empty
-    distinct_achivements = set().union(*persons.values())
-    
-    print(f"There is {len(distinct_achivements)} distincts achivements")
-    print(distinct_achivements)
+    distinct_achievements = set().union(*persons.values())
+
+    print(f"There is {len(distinct_achievements)} distincts achievements")
+    print(distinct_achievements)
     print("======")
 
-    all_people_achivements: list[set] = list(persons.values())
-    
-    # Uniqueness
-    for person, achivement in persons.items():
+    all_people_achievements: list[set] = list(persons.values())
+
+    for person, achievement in persons.items():
         # print(f"Only {person.capitalize()} has :", end='')
-        print(f"Current {person.capitalize()} achivement = {achivement}\n")
-        # print(f"All people achivemment : {all_people_achivements}")
-        
-        # Removed current achivements to all people achivement
-        cleanned_achivements = [x for x in all_people_achivements if achivement != x]
-        # print(f"Cleanned = {cleanned_achivements}")
-        
-        diff = set(achivement).difference(*cleanned_achivements)
-        
-        if diff ==set():
-            print(f"Player {person.capitalize()} has no unique achivement")
+        print(f"\nCurrent {person.capitalize()} has {len(achievement)} achievement(s) = {achievement}")
+        # print(f"All people achivemment : {all_people_achievements}")
+
+        # ! STEP 1 : Track Uniqueness
+        # Removed current person's achievement to all people achievement
+        cleanned_achievements = [
+            x for x in all_people_achievements if achievement != x
+        ]
+
+        diff = set(achievement).difference(*cleanned_achievements)
+        if diff == set():
+            # print(f"Player {person.capitalize()} has no unique achievement")
+            print(
+                f"\033[31mPlayer {person.capitalize()} has no unique achievement\033[0m"
+            )
         else:
-            print(f"Player {person.capitalize()} has unique achivements = {diff}")
+            # print(f"")
+            print(
+                f"\033[32mPlayer {person.capitalize()} has unique achievements = {diff}\033[0m"
+            )
+        # ! STEP 2 : Track Missing achievements
+        missing_achievements = set(distinct_achievements).difference(achievement)
 
-    
-    
+        if missing_achievements == set():
+            print(f"\033[32m{person.capitalize()} has all achievements\033[0m")
+        else:
+            print(f"\033[31m{person.capitalize()} miss {len(missing_achievements)} achievements : {missing_achievements} \033[0m")
 
-    
+    # print(f"All people achievements = {all_people_achievements}")
+    # ! Find achievements shared by the players
+    intersection = set.intersection(*all_people_achievements)
+
+    # ! Important : `set().intersection` will always compare null with something else,
+    # ! which will ultimately return null.
+    # intersection_parenthesis = set().intersection(*all_people_achievements)
+    if intersection == set():
+        print(f"\n\033[31mThere is no common achievements\033[0m")
+    else:
+        print(f"\n\033[32mCommon achievements = {intersection}\033[0m")
 
 
 def main() -> None:
@@ -79,14 +100,14 @@ def main() -> None:
     print("=== Achievement Tracker System ===")
 
     persons: dict[str, set] = {
-        # "alice": gen_player_achievements(),
-        "alice": {"test1", "test2"},
+        "alice": gen_player_achievements(),
         "bob": gen_player_achievements(),
         "chalie": gen_player_achievements(),
         "dylan": gen_player_achievements(),
     }
 
     display_stats(persons)
+
 
 if __name__ == "__main__":
     main()
