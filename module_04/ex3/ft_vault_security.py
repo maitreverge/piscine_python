b6e75a29c5cd1b25bb012fe1e2d9ad3391c5f44f
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-05 18:24:53 +0200

    Done module 04

--- module_04/ex3/ft_vault_security.py
+++ module_04/ex3/ft_vault_security.py
@@ -1,53 +1,74 @@
 #! /usr/bin/python3
 """
-_module_doc_
+This modules emphasizes the importance of handling exceptions
+and using the `with` statement for file operations.
 """
 
 
 def secure_archive(
     file_name: str, file_mode: str = "r", content: str = ""
 ) -> tuple[bool, str]:
+    """
+    Securely archives a file with the specified mode and content.
+
+    Args:
+        file_name (str): The name of the file to archive.
+        file_mode (str, optional): The mode to open the file. Defaults to "r".
+        content (str, optional): The content to write. Defaults to "".
+
+    Returns:
+        tuple[bool, str]: A tuple with the success and the data/error message.
+    """
     operation_success: bool = False
     data: str = ""
-    str_info = ""
+    str_info = "regular"
+    action: str = file_mode.lower().strip()
     try:
         # Check file_mode arg
-        action: str = file_mode.lower().strip()
-        assert action in "r" or action == "w"
+        assert action in ["r", "w"]
 
-        with open(file_name, file_mode, encoding="utf-8") as file:
-            if file_mode == "r":
+        with open(file=file_name, mode=action, encoding="utf-8") as file:
+            if action == "r":
                 data = file.read()
+                operation_success = True
             else:
-                assert content != ""
                 file.write(content)
+                data = "Content successfully written to file"
+                operation_success = True
 
+    except FileNotFoundError as e:
+        data = f"{e}"
+        str_info = "non-existing"
+    except PermissionError as e:
+        data = f"{e}"
+        str_info = "non-writable"
+    except AssertionError as e:
+        data = f"{e}"
+        str_info = "wrong mode"
     except Exception as e:
         data = f"{e}"
+        str_info = "unknown"
 
-    finally:
-        print(
-            f"Using {file_name} to {"read" if file_mode == "r" else "write"}",
-            end="",
-        )
-        print(f"from a {str_info} file: ")
-        return (operation_success, data)
+    operation = "read" if action == "r" else "write"
+    print(f"Using `{file_name}` to {operation} ", end="")
+    print(f"from a {str_info} file: ")
+    return (operation_success, data)
 
 
 def main() -> None:
     """
     Main function
     """
-    file_name: str = "data.txt"
-    # file_name = "dataaaaaaaaaa.txt"
-    file_mode: str = "r"
+    # file_name: str = "./data.txt"
+    # file_name: str = "data.txt"
+    file_name = "dataaaaaaaaaa.txt"
+    # file_mode: str = "r"
     # file_mode : str = "w"
-    # file_mode = "R   "
+    file_mode: str = "R   "
     # file_mode = "   W   "
     # file_mode = "hahahaha"
     content: str = "Content very interesting"
-    # content: str = ""
-    secure_archive(file_name, file_mode, content)
+    print(secure_archive(file_name, file_mode, content))
 
 
 if __name__ == "__main__":
