85cfffb369568259de8ca154973822c524baf20b
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-07 11:22:51 +0200

    fuck this exercice

--- module_05/ex0/data_processor.py
+++ module_05/ex0/data_processor.py
@@ -1,40 +1,76 @@
 #! /usr/bin/python3
-from abc import ABC, abstractmethod
-from typing import Any
+"""
+This modules focuses on the use of abstract base classes (ABCs)
 
-"""
-This modules focuses on the use of abstract base classes (ABCs) 
-"""
-
-"""
 Notes :
 Abstract Class : can't be instancianted, raises a TypeError.
 Serves as a blueprint
 """
 
+from abc import ABC, abstractmethod
+from typing import Any
+
 
 class DataProcessor(ABC):
+    """
+    _Base Abstract Class, used for blueprinting other classes_
+
+    Args:
+        ABC (_type_): _Base Abstract Class_
+    """
+
     def __init__(self) -> None:
+        """
+        Init fuction.
+        `self._processed_data` -> Store main parsed data.
+        `self._processing_rank` -> Store order in which data has been stored
+        """
         super().__init__()
         self._processed_data: list[tuple[int, str]] = []
         self._processing_rank: int = 0
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
-        pass
+        """
+        _Abstract Method for validating data_
+
+        Args:
+            data (Any): _raw data to parse_
+
+        Returns:
+            bool: _returns `True` is matches format, `False` otherwise_
+        """
 
     @abstractmethod
     def ingest(self, data: Any) -> None:
-        pass
+        """
+        _Abstract Method for ingesting data_
+
+        Args:
+            data (Any): _raw data to save_
+        """
 
     # No need to override this method
-    def output(self) -> tuple[int, str]: ...
+    def output(self) -> tuple[int, str]:
+        """
+        Pop the tuple at index `[0]` from `self._processed_data`, and return it
+
+        Returns:
+            tuple[int, str]: _self._processed_data.pop(0)_
+        """
+        return self._processed_data.pop(0)
 
 
 class NumericProcessor(DataProcessor):
-    def __init__(self) -> None:
-        super().__init__()
+    """
+    _Child class of `DataProcessor`, used for parsing and saving
+    numeric data_
 
+    Args:
+        DataProcessor (_type_): _Base Abstract Class `DataProcessor_
+    """
+
+    # ! Note : No need of __init__ method
     def validate(self, data: Any) -> bool:
         """
         Parse if `data` is of type `int`, `float`, `list[int]`, `list[float]`
@@ -48,7 +84,7 @@ class NumericProcessor(DataProcessor):
         """
         if isinstance(data, (int, float)):
             return True
-        elif isinstance(data, list):
+        if isinstance(data, list):
             # Check the whole list first
             for item in data:
                 if not isinstance(item, (int, float)):
@@ -67,6 +103,7 @@ class NumericProcessor(DataProcessor):
             data (list | dict): _data to ingest_
         """
         if self.validate(data):
+            # Check if data is either a simple `int` or `float`
             if isinstance(data, (int, float)):
                 to_append = (self._processing_rank, str(data))
                 self._processed_data.append(to_append)
@@ -82,8 +119,13 @@ class NumericProcessor(DataProcessor):
 
 
 class TextProcessor(DataProcessor):
-    def __init__(self) -> None:
-        super().__init__()
+    """
+    _Child class of `DataProcessor`, used for parsing and saving
+    text data_
+
+    Args:
+        DataProcessor (_type_): _Base Abstract Class `DataProcessor_
+    """
 
     def validate(self, data: Any) -> bool:
         """
@@ -97,7 +139,7 @@ class TextProcessor(DataProcessor):
         """
         if isinstance(data, str):
             return True
-        elif isinstance(data, list):
+        if isinstance(data, list):
             # Check the whole list first
             for item in data:
                 if not isinstance(item, str):
@@ -115,18 +157,28 @@ class TextProcessor(DataProcessor):
         if self.validate(data):
             if isinstance(data, str):
                 # self._processed_data.append(data)
-                to_append = (self._processing_rank, data)
+                to_append = (self._processing_rank, data.strip())
                 self._processed_data.append(to_append)
                 self._processing_rank += 1
             else:
                 for item in data:
-                    self._processed_data.append((self._processing_rank, item))
+                    self._processed_data.append(
+                        (self._processing_rank, item.strip())
+                    )
                     self._processing_rank += 1
         else:
             raise TypeError("Incorrect Provided Data.")
 
 
 class LogProcessor(DataProcessor):
+    """
+    _Child class of `DataProcessor`, used for parsing and saving
+    log data_
+
+    Args:
+        DataProcessor (_type_): _Base Abstract Class `DataProcessor_
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self._log_levels = [
@@ -160,7 +212,10 @@ class LogProcessor(DataProcessor):
             if not isinstance(key, str) or not isinstance(value, str):
                 return False
         # Check that keys matches this format
-        if list(data) != ["log_level", "log_message"]:
+        if list(data) != ["log_level", "log_message"] or list(data) != [
+            "log_message",
+            "log_level",
+        ]:
             return False
         # Check that first key stripped and UPPER matches the log levels
         if list(data.values())[0].strip().upper() not in self._log_levels:
@@ -183,9 +238,10 @@ class LogProcessor(DataProcessor):
             return self._validate_dict(data)
         return False
 
-    def _ingest_dict(self, data: dict) -> None:
+    def _ingest_dict(self, data: dict[str, str]) -> None:
         """
-        Create a tuple with combined `log_level` and `log_message` and append it to `self._processed_data`
+        Create a tuple with combined `log_level` and `log_message` and append
+        it to `self._processed_data`
 
         Args:
             data (dict): _dict data_
@@ -197,7 +253,7 @@ class LogProcessor(DataProcessor):
         )
         self._processing_rank += 1
 
-    def ingest(self, data: list | dict) -> None:
+    def ingest(self, data: list[dict[str, str]] | dict[str, str]) -> None:
         """
         Ingest data to `self._processed_data`
 
@@ -211,11 +267,112 @@ class LogProcessor(DataProcessor):
             else:
                 self._ingest_dict(data)
 
+def test_numeric(true_data: list, false_data: )
 
 def main() -> None:
     """
     Main function
     """
+    authorized_log_levels = [
+        "EMERGENCY",
+        "ALERT",
+        "CRITICAL",
+        "ERROR",
+        "WARNING",
+        "NOTICE",
+        "INFO",
+        "DEBUG",
+    ]
+
+    numeric_data: list = [
+        42,
+        0,
+        -42,
+        42.42,
+        0.1,
+        -42.42,
+        [42, -42],
+        [42.42, -42.42],
+        [42, 42.42],
+        [42, 42.42, 42, 0, 0.998, 100000],
+    ]
+
+    text_data = [
+        "test",
+        "test2",
+        "",
+        "t",
+        ["t"],
+        ["t", "test"],
+        ["t", "test"],
+        ["t", "test", "test,4"],
+    ]
+
+    log_data = [
+        # Simple dicts
+        {"log_level": "NOTICE", "log_message": "This is a message of notice"},
+        {
+            "log_message": "This is a message of warning",
+            "log_level": "WarNinG",
+        },
+        # Build all authorized log possibilities in a list of all dicts
+        [
+            {"log_level": log, "log_message": f"Message of {log}"}
+            for log in authorized_log_levels
+        ],
+    ]
+
+    mixed_numeric_data: list = [
+        42,
+        0,
+        -42,
+        "test2",
+        "",
+        "t",
+        ["t"],
+        42.42,
+        0.1,
+        -42.42,
+        [42, -42],
+        [42.42, -42.42],
+        [42, 42.42],
+        [
+            {"log_level": log, "log_message": f"Message of {log}"}
+            for log in authorized_log_levels
+        ],
+    ]
+
+    mixed_text_data = [
+        "test",
+        "test2",
+        -42.42,
+        [42, -42],
+        "",
+        "t",
+        ["t"],
+        ["t", "test"],
+        {
+            "log_message": "This is a message of warning",
+            "log_level": "WarNinG",
+        },
+        ["t", "test"],
+        ["t", "test", "test,4"],
+    ]
+
+    mixed_log_data = [
+        [
+            {"log_level": log, "log_message": f"Message of {log}"}
+            for log in authorized_log_levels
+        ],
+        {"log_level": "NOTICE", "log_message": "This is a message of notice"},
+        "t",
+        ["t", "test", "test,4"],
+        42.42,
+        0.1,
+        -42.42,
+        [42, -42],
+    ]
+
 
 
 if __name__ == "__main__":
