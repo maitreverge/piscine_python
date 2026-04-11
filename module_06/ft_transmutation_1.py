c25e447f0b24a92513b1f0197e6c4b1c75d2a9d6
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-11 18:45:51 +0200

    trans_1

---
+++ module_06/ft_transmutation_1.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+"""
+_module_doc_
+"""
+
+import alchemy.transmutation
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    print("=== Transmutation 1 ===")
+    print("Import transmutation module directly")
+    print(f"Testing lead to gold: {alchemy.transmutation.lead_to_gold()}")
+
+
+if __name__ == "__main__":
+    main()
