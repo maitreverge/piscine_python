b967d19b8a1a673c07e31df138608e7d2f12f282
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-11 18:49:32 +0200

    trans ok

--- module_06/alchemy/__init__.py
+++ module_06/alchemy/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["create_air", "strength_potion", "heal"]
+__all__ = ["create_air", "strength_potion", "heal", "lead_to_gold"]
 
 # ft_alembic_4.py
 from .elements import create_air
@@ -6,3 +6,6 @@ from .elements import create_air
 # ft_distilliation_1.py
 from .potions import strength_potion
 from .potions import healing_potion as heal
+
+# ft_transmutation_2.py
+from .transmutation import lead_to_gold
---
+++ module_06/ft_transmutation_2.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+"""
+_module_doc_
+"""
+
+import alchemy
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    print("=== Transmutation 2 ===")
+    print("Import transmutation module directly")
+    print(f"Testing lead to gold: {alchemy.lead_to_gold()}")
+
+
+if __name__ == "__main__":
+    main()
