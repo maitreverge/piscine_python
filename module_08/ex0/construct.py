e78b254ba6ae00cc17b6ffb4afeae147f8390aaa
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-04-27 21:14:30 +0200

    harry potter type of code

---
+++ module_08/ex0/construct.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python3
+"""
+_module_doc_
+"""
+
+import site, sys, os
+
+def is_virtual_env_os() -> bool:
+    """
+    Detects virtual env by getting the `VIRTUAL_ENV`
+
+    # ! NOTE : This function is here for demonstrating getting this piece of
+    # ! info by another module. This solution is the less relaible, since
+    # ! shell env can be modified elsewhere in the system.
+
+    Returns:
+        bool: _Is the programs runs in a venv_
+    """
+    result = os.getenv("VIRTUAL_ENV")
+    return result is not None
+
+def is_virtual_env_sys() -> bool:
+    """
+    Detects virtual env by comparing `sys.base_prefix != sys.prefix`
+    False otherwise.
+
+    Returns:
+        bool: _Is the programs runs in a venv_
+    """
+    return sys.base_prefix != sys.prefix
+
+def explain_env() -> None:
+    """
+    _Explain to muggle people hot to create a virtual env_
+    """
+    print("Program DOES NOT runs in a virtual env")
+    print("===")
+    print("Run the command\n\npython3 -m venv `name_of_virtual_environment")
+    print("Example:\n\tpython3 -m venv .venv")
+    print("===")
+    print("Welcome to Hogwarts 🪄\n")
+
+def main() -> None:
+    """
+    Main function
+    """
+
+    if is_virtual_env_sys():
+        print("Program runs in a virtual env")
+    else:
+        explain_env()
+        
+    
+    print(f"Current Python binary is located at `{sys.prefix}`")
+
+    print(f"Those are the installed dependencies in this location :")
+    print(site.getsitepackages())
+    
+
+if __name__ == "__main__":
+    main()
